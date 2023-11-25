9fa11320a33510e6699ee8dd4472d1746bbc0c2f
Runze-Z <148878112+Runze-Z@users.noreply.github.com>
2023-11-25 10:00:31 +0100

    Update script.js

--- public/script.js
+++ public/script.js
@@ -3,55 +3,40 @@ function toggleAccordion(button) {
     content.style.display = content.style.display === 'block' ? 'none' : >
     button.children[1].classList.toggle('rotate');
 }
-// 使用 fetch 发送请求到后端
+
+// Mit fetch eine Anfrage an den Backend senden
 fetch('http://34.79.116.218:8080/')
     .then(response => response.json())
     .then(data => {
-        // 使用返回的数据来创建手风琴项
+        // Mit den zurückgegebenen Daten Akkordeon-Elemente erstellen
         createAccordionItems(data);
     })
     .catch(error => console.error('Error:', error));
 
-// 创建手风琴项的函数
-function toggleAccordion(button) {
-    const content = button.nextElementSibling;
-    content.style.display = content.style.display === 'block' ? 'none' : >
-    button.children[1].classList.toggle('rotate');
-}
-
-// 使用 fetch 发送请求到后端
-fetch('http://34.79.116.218:8080/')
-    .then(response => response.json())
-    .then(data => {
-        // 使用返回的数据来创建手风琴项
-        createAccordionItems(data);
-    })
-    .catch(error => console.error('Error:', error));
-
-// 创建手风琴项的函数
+// Eine Funktion zum Erstellen von Akkordeon-Elementen
 function createAccordionItems(data) {
     const accordion = document.querySelector('.accordion');
-    accordion.innerHTML = ''; // 清空现有的内容
+    accordion.innerHTML = ''; // Den vorhandenen Inhalt leeren
 
     data.forEach(item => {
-        // 创建手风琴项目
+        // Akkordeon-Elemente erstellen
         const accordionItem = document.createElement('div');
         accordionItem.className = 'accordion-item';
-        // 创建手风琴按钮
+        // Akkordeon-Tasten erstellen
         const accordionButton = document.createElement('button');
         accordionButton.className = 'accordion-button';
         accordionButton.onclick = function() { toggleAccordion(this); };
         accordionButton.innerHTML = `<div class="accordion-title">${item.>
 
-        // 创建手风琴内容
+        // Akkordeon-Inhalte erstellen
         const accordionContent = document.createElement('div');
         accordionContent.className = 'accordion-content';
         accordionContent.innerHTML = `<p>${item.answer}</p>`;
 
-        // 将按钮和内容添加到手风琴项目中
+        // Knöpfe und Inhalte zu Akkordeon-Elementen hinzufügen
         accordionItem.appendChild(accordionButton);
         accordionItem.appendChild(accordionContent);
 
-        // 将手风琴项目添加到手风琴容器中
+        // Akkordeon-Elemente zum Akkordeon-Container hinzufügen
         accordion.appendChild(accordionItem);
     });
